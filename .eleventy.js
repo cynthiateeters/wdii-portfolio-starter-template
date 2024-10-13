aeeecf160d133c2eccb80e49cecdbaff2dba0296
Cynthia Teeters <cynthiateeters@gmail.com>
2024-10-13 15:30:35 -0400

    Fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,15 @@
 const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
+  // watch these for changes
+  eleventyConfig.addWatchTarget("./src/assets/");
+  eleventyConfig.addWatchTarget("./src/fonts/");
+  eleventyConfig.addWatchTarget("./src/images/");
   eleventyConfig.addWatchTarget("./src/sass/");
+  // copy these folders to public folder
   eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/fonts');
+  eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/admin');
 
